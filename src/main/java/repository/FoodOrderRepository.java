6c2df447cb01c72df2d85314d3a017bf07ba1392
samira shokrane <shokrane.samira@gmail.com>
2026-06-27 11:25:04 +0330

    main-

--- src/main/java/repository/FoodOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.foodsystem.food_order_app.repository;
-
-import com.foodsystem.food_order_app.model.FoodOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodOrderRepository extends JpaRepository<FoodOrder, Long> {
-}
--- src/main/java/repository/FoodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foodsystem.food_order_app.repository;
-
-import com.foodsystem.food_order_app.model.Food;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FoodRepository extends JpaRepository<Food, Long> {
-}
